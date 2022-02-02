f286c28c1a68b059905d2f5fd87b4cc204a393bf
Seevii16 <sevillanito435@gmail.com>
2022-02-02 19:04:36 +0100

    Primer commit TailWind

---
+++ SegundoTrimestre/TailWind/PrimerProyecto/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                "azul-claro": "#37bcf9",
+                "azul-oscuro": "#0370b9"
+            },
+            animation: {
+                'spin-slow': 'spin 3s linear infinite',
+                
+
+            }
+        },
+    },
+    plugins: [],
+}
